0b6915fa1e7d8662ee939fddc858130ba3a1ee80
HornedHeck <holitrixpecora@gmail.com>
2021-01-31 15:26:32 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "com.hornedheck"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
